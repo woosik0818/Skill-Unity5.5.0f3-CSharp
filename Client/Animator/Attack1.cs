96e8cc5009f0f3600205b17e3e1dffd8f75a6f83
Chris_Lim <woosik0818@hanmail.net>
2018-03-26 19:55:29 +0900

    Update Attack1.cs

--- Client/Animator/Attack1.cs
+++ Client/Animator/Attack1.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Attack1 : StateMachineBehaviour 
 {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
-    {
+    	{
         
 	}
 }
